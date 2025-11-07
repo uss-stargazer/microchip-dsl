afcca8687f5195fd5c867369927b682fa6a43382
anonymous <anonymous>
2025-11-06 22:11:14 -0500

    feat(json): Implement runtime type checking for string to state

--- src/json.ts
+++ src/json.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { MicrochipState } from './microchip.js';
+import { Component } from './component.js';
 
 // parital credit: https://stackoverflow.com/a/56150320 (for replacer and reviver)
 
@@ -46,12 +47,47 @@ function microchipStateReviver(key: any, value: any): any {
   return value;
 }
 
-export function microchipStateToJsonStr(state: MicrochipState) {
+export function microchipStateToJsonStr(state: MicrochipState): string {
   return JSON.stringify(state, microchipStateReplacer);
 }
 
-export function microchipStateFromJsonStr(data: string) {
-  return JSON.parse(data, microchipStateReviver);
+export function microchipStateFromJsonStr(data: string): MicrochipState {
+  const microchipState = JSON.parse(data, microchipStateReviver);
+  if (
+    !(
+      typeof microchipState.rootComponent === 'number' &&
+      microchipState.componentRegistry instanceof Map &&
+      [
+        ...(
+          microchipState.componentRegistry as Map<number, Component>
+        ).entries(),
+      ].every(
+        (entry) =>
+          typeof entry[0] === 'number' &&
+          typeof entry[1].nInputs === 'number' &&
+          typeof entry[1].nOutputs === 'number' &&
+          (typeof entry[1].state === 'string' ||
+            (typeof entry[1].state.components === 'object' &&
+              entry[1].state.components.every((id) => typeof id === 'number') &&
+              entry[1].state.connections instanceof Set &&
+              [...entry[1].state.connections.values()].every((connection) =>
+                [connection.source, connection.destination].every(
+                  (signal) =>
+                    typeof signal === 'object' &&
+                    (typeof signal.component === 'number' ||
+                      signal.component === null ||
+                      signal.component === 'input' ||
+                      signal.component === 'output') &&
+                    (typeof signal.pin === 'number' || signal.pin === null),
+                ),
+              ))) &&
+          typeof entry[1].style === 'object',
+      )
+    )
+  ) {
+    throw new Error('Object does not extend MicrochipState interface');
+  }
+  return microchipState;
 }
 
 export function microchipStateToJson(
