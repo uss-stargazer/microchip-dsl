261ec49b9a5dd2e0c63b27bcc255196ef3ccb9cb
anonymous <anonymous>
2025-11-06 22:15:20 -0500

    fix: Make all module exports available from index, so user can use single import if desired

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { Microchip, type MicrochipState } from './microchip.js';
+export { type Signal, nullSignal, copySignal } from './signal.js';
+
+// Just to package everything into one import if desired
+export * as microchip from './microchip.js';
+export * as component from './component.js';
+export * as signal from './signal.js';
+export * as json from './json.js';
+export * as utils from './utils.js';
